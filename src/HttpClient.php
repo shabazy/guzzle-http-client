64d80e421838884ee3c07832ad1751be57d0b699
Gökhan ENSEP <gokhan.ensep@metglobal.com>
2019-07-06 13:40:51 +0300

    refactored guzzle client for all methods

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -6,6 +6,10 @@ use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 use GuzzleHttp\Exception\ClientException;
 
+/**
+ * Class HttpClient
+ * @package GuzzleHttpClient
+ */
 class HttpClient {
 
 	public $client;
@@ -13,8 +17,12 @@ class HttpClient {
 	public $password;
 	public $options;
 
-	public function __construct($options = []) {
-
+    /**
+     * HttpClient constructor.
+     * @param array $options
+     */
+	public function __construct(array $options = [])
+    {
 		$this->client   = new Client();
 		$this->username = $options['username'];
 		$this->password = $options['password'];
@@ -23,30 +31,49 @@ class HttpClient {
 		];
 	}
 
-	public function setHeaders() {
-		$this->options[RequestOptions::HEADERS] = ['Content-Type' => 'application/x-www-form-urlencoded'];
+    /**
+     * @param array $option
+     * @return $this
+     */
+	public function addOption(array $option)
+    {
+		$this->options = $option;
+
+		return $this;
 	}
 
-	public function makeRequest($url, $method = 'get', $payload = []) {
-
+    /**
+     * @param $url
+     * @param string $method
+     * @param array $payload
+     * @return \Psr\Http\Message\ResponseInterface|null
+     * @throws \Exception
+     */
+	public function makeRequest(string $method, string $url, $payload = [])
+    {
 		if (empty($url)) {
 			throw new \Exception('URL not found');
 		}
 
 		if ($method == 'get' && !empty($payload)) {
 			$url .= '?' . http_build_query($payload);
-		} else if ($method == 'post') {
-			$this->options[RequestOptions::FORM_PARAMS] = $payload;
-			$this->setHeaders();
 		}
+		else if (in_array($method, ['post', 'put', 'delete'])) {
+			$this->options[RequestOptions::JSON] = $payload;
+		}
+		else {
+		    throw new \Exception('Method not found.');
+        }
 
 		$method = strtolower($method);
 
 		try {
 			$response = $this->client->$method($url, $this->options);
-		} catch (ClientException $e) {
+		}
+		catch (ClientException $e) {
 			$response = $e->getResponse();
-		} catch (\Exception $e) {
+		}
+		catch (\Exception $e) {
 			throw new \Exception($e->getMessage(), $e->getCode());
 		}
 
